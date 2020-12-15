46288a561c37710cf9071ad465aeb21445c14bd5
seth <seth@ske.moe>
2020-12-15 08:02:56 +0000

    API: enable omnimix for only MUSECA and IIDX

--- bemani/api/objects/records.py
+++ bemani/api/objects/records.py
@@ -222,8 +222,11 @@ class RecordsObject(BaseObject):
 
     @property
     def music_version(self) -> int:
-        if self.omnimix:
-            return self.version + DBConstants.OMNIMIX_VERSION_BUMP
+        if self.game in [GameConstants.IIDX, GameConstants.MUSECA]:
+            if self.omnimix:
+                return self.version + DBConstants.OMNIMIX_VERSION_BUMP
+            else:
+                return self.version
         else:
             return self.version
 
